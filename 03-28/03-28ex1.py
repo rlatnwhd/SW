79b7776adb485dea3ced9d57dd3892d5e6707c5c
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2023-04-04 13:14:26 +0900

    03-28

---
+++ 03-28/03-28ex1.py
@@ -0,0 +1,17 @@
+'''
+2023.03.28
+김수종
+연산자 연습
+'''
+num1=10    #num1 변수에 정수 10 지정
+num2=5     #num2 변수에 정수 5 지정
+
+res1=num1*num2 #res1 변수에 num1*num2 경과 저장
+res2=num1/num2 #res2 변수에 num1/num2 경과 저장
+res3=num1//num2 #res3 변수에 num1//num2 경과 저장
+res4=num1%num2 #res4 변수에 num1%num2 경과 저장
+
+print("num1 * num2 =", res1)
+print("num1 / num2 =", res2)
+print("num1 // num2 =", res3)
+print("num1 % num2 =", res4)

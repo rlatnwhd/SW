3b1dbe34443ad72c58d1eef5c5089b269da9841b
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2023-04-04 13:14:09 +0900

    3-21~22

---
+++ 03-22/03-21ex2.py
@@ -0,0 +1,7 @@
+first=input("input num1 : ") #첫번째 정수 입력
+second=input("input num2 : ") #두번째 정수 입력
+
+if first>second: #선택 조건
+    print("Big number is :",first)
+else:
+    print("Big number is :",second)
